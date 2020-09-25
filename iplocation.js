caca55f9592042e9d72fe05ed9c0f22ac349fcfd
Gustavo Lira <gustavolirasn@gmail.com>
2020-09-25 11:58:37 -0300

    Update iplocation.js

--- iplocation.js
+++ iplocation.js
@@ -2,11 +2,11 @@ const readline = require('readline-sync')
 
 function start(){
     const ipUser = {}
-    ipUser.gettinip = getIp()
-
+    
     function getIp() {
         return readline.question("Qual o ip que deseja rastrear? ")}
     var geoip = require('geoip-lite')
     var geo = geoip.lookup(ipUser.gettinip)
     console.log(geo)}
+ipUser.gettinip = getIp()
 start()
